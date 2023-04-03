8dd6e30a8e5439ddd15133c533fbe4f778804d93
DESKTOP-C03SHVG\facto <factorio@naver.com>
2023-04-03 19:53:46 +0900

    Add: QR out

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,8 @@ import 'package:lcqm/login.dart';
 import 'package:motion_toast/motion_toast.dart';
 import 'firebase_options.dart';
 import 'in.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+import 'dart:async';
 
 void main() async {
   islogin();
@@ -114,21 +117,24 @@ class _HomePageState extends State<HomePage> {
                                 .roundToDouble())),
                   ),
                 ),
- 
                 SizedBox(
                   width: MediaQuery.of(context).size.width * width,
                   height: MediaQuery.of(context).size.height * height,
                   child: ElevatedButton(
                     onPressed: () {
-                      MotionToast.info(
-                        title: const Text(
-                          '현재 개발중',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        description: const Text('현재 개발중인 피쳐입니다.🛠'),
-                      ).show(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Qrscan()),
+                      );
+                      // MotionToast.info(
+                      //   title: const Text(
+                      //     '현재 개발중',
+                      //     style: TextStyle(
+                      //       fontWeight: FontWeight.bold,
+                      //     ),
+                      //   ),
+                      //   description: const Text('현재 개발중인 피쳐입니다.🛠'),
+                      // ).show(context);
                     },
                     style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all<Color>(
@@ -145,7 +151,6 @@ class _HomePageState extends State<HomePage> {
                                 .roundToDouble())),
                   ),
                 ),
- 
                 SizedBox(
                   width: MediaQuery.of(context).size.width * width,
                   height: MediaQuery.of(context).size.height * height,
@@ -172,7 +177,6 @@ class _HomePageState extends State<HomePage> {
                                 .roundToDouble())),
                   ),
                 ),
-
                 SizedBox(
                   width: MediaQuery.of(context).size.width * width,
                   height: MediaQuery.of(context).size.height * height,
@@ -214,7 +218,8 @@ class _HomePageState extends State<HomePage> {
                             TextButton(
                                 onPressed: () {
                                   Navigator.pop(context);
-                                }, child: const Text("취소")),
+                                },
+                                child: const Text("취소")),
                             TextButton(
                                 onPressed: () {
                                   final box = Hive.box("auth");
@@ -225,8 +230,7 @@ class _HomePageState extends State<HomePage> {
                                 child: const Text("로그아웃"))
                           ],
                         );
-                      }
-                      );
+                      });
                 },
                 child: IconButton(
                   onPressed: () {
@@ -258,3 +262,168 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+class Qrscan extends StatefulWidget {
+  const Qrscan({Key? key}) : super(key: key);
+
+  @override
+  State<Qrscan> createState() => _QrscanState();
+}
+
+class _QrscanState extends State<Qrscan> {
+  String? _output = 'Empty Scan Code';
+  final TextEditingController _amountController = TextEditingController();
+  Map<String, dynamic> headers = {};
+  Dio dio = Dio();
+
+  @override
+  initState() {
+    super.initState();
+    setState(() {
+      final box = Hive.box("auth");
+      headers = {"psk": box.get('psk', defaultValue: "null")};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('QR 스캔'),
+        ),
+        backgroundColor: Colors.grey[300],
+        body: Builder(
+          builder: (BuildContext context) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("스캔된 모델명: $_output",
+                      style: TextStyle(color: Colors.black, fontSize: (((MediaQuery.of(context).size.width +
+                          MediaQuery.of(context)
+                              .size
+                              .height) *
+                          0.001) *
+                          20)
+                          .roundToDouble())),
+                  const SizedBox(height: 15,),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * width,
+                    height: MediaQuery.of(context).size.height * height / 2,
+                    child: TextField(
+                      controller: _amountController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '수량',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                SizedBox(
+                width: 150,
+                height: 80,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                        MaterialStateProperty.all<
+                            Color>(
+                            const Color.fromRGBO(
+                                241, 27, 35, 1.0))),
+                    onPressed: () async {
+                      final res = await dio.get(
+                          "$baseuri:443/out/1/$_output/${_amountController.value.text}",
+                          options:
+                          Options(headers: headers));
+                      if (res.statusCode == 200) {
+                        outSuccess(context);
+                      } else {
+                        outError(context);
+                      }
+                    },
+                    child: const Text("출고\n(세트)",
+                        textAlign: TextAlign.center)),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              SizedBox(
+                width: 150,
+                height: 80,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                        MaterialStateProperty.all<
+                            Color>(
+                            const Color.fromRGBO(
+                                241, 27, 35, 1.0))),
+                    onPressed: () async {
+                      final res = await dio.get(
+                          "$baseuri:443/out/0/$_output/${_amountController.value.text}",
+                          options:
+                          Options(headers: headers));
+                      if (res.statusCode == 200) {
+                        outSuccess(context);
+                      } else {
+                        outError(context);
+                      }
+                    },
+                    child: const Text(
+                      "출고\n(개별)",
+                      textAlign: TextAlign.center,
+                    )),),],),
+                ],
+              ),
+            );
+          },
+        ),
+        //플로팅 액션 버튼으로 qr 스캔 함수 실행
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _scan(),
+          tooltip: 'scan',
+          child: const Icon(Icons.camera_alt),
+        ),
+      ),
+    );
+  }
+
+  //비동기 함수
+  Future _scan() async {
+    //스캔 시작 - 이때 스캔 될때까지 blocking
+    String? barcode = await scanner.scan();
+    //스캔 완료하면 _output 에 문자열 저장하면서 상태 변경 요청.
+    setState(() => _output = barcode);
+  }
+}
+
+void outError(BuildContext context) {
+  MotionToast.error(
+    title: const Text(
+      '출고 실패',
+      style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    description: const Text(
+      '출고에 실패하였습니다.',
+      style: TextStyle(fontSize: 12),
+    ),
+    dismissable: true,
+  ).show(context);
+}
+
+void outSuccess(BuildContext context) {
+  MotionToast.success(
+    title: const Text(
+      '출고 완료',
+      style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    description: const Text(
+      '출고가 성공적으로 완료되었습니다.',
+      style: TextStyle(fontSize: 12),
+    ),
+    dismissable: true,
+  ).show(context);
+}
+
